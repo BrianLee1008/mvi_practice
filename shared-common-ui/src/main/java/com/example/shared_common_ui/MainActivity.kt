1df8dea939aea8c136669903fed31ee8891e94c0
BrianLee <tglee951008@gmail.com>
2022-08-02 23:11:40 +0900

    Appcompat -> Component

--- shared-common-ui/src/main/java/com/example/shared_common_ui/MainActivity.kt
+++ shared-common-ui/src/main/java/com/example/shared_common_ui/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.shared_common_ui
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
